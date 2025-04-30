44554956cf550d8661ebccdce6dcab0890ac0c6d
abdulhakim10 <s.abdulhakim10@gmail.com>
2025-04-30 17:37:59 +0800

    problem solved

--- problem_solving.c
+++ problem_solving.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     // 1. Welcome with Conditons
@@ -24,13 +24,85 @@ int main()
 
     // 3. First digit!
 
-    int n;
-    scanf("%d", &n);
-    n = n % 10;
-    if(n % 2 == 0){
-        printf("Even");
-    }else{
-        printf("Odd");
+    // int n;
+    // scanf("%d", &n);
+    // n = n / 1000;
+    // if(n % 2 == 0){
+    //     printf("Even");
+    // }else{
+    //     printf("Odd");
+    // }
+
+    // Last digit!
+
+    // int n;
+    // scanf("%d", &n);
+    // n = n % 10;
+    // if(n % 2 == 0){
+    //     printf("Even");
+    // }else{
+    //     printf("Odd");
+    // }
+
+    // 4. Char
+
+    // char x;
+    // scanf("%c", &x);
+    // if (x >= 'a' && x <= 'z')
+    // {
+    //     x = x - 32;
+    //     printf("%c", x);
+    // }
+    // else if (x >= 'A' && x <= 'Z')
+    // {
+    //     x = x + 32;
+    //     printf("%c", x);
+    // }
+
+    // 5. Capital or Small or Digit
+
+    // char x;
+    // scanf("%c", &x);
+    // if(x >= '0' && x <= '9'){
+    //     printf("IS DIGIT");
+    // } else{
+    //     printf("ALPHA\n");
+    //     if(x >= 'A' && x <= 'Z'){
+    //         printf("IS CAPITAL");
+    //     }else{
+    //         printf("IS SMALL");
+    //     }
+    // }
+
+    // 6. Max and Min
+
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    // minimum
+    if (a <= b && a <= c)
+    {
+        printf("%d ", a);
+    }
+    else if (b <= a && b <= c)
+    {
+        printf("%d ", b);
+    }
+    else if (c <= a && c <= b)
+    {
+        printf("%d ", c);
+    }
+    // maximum
+    if (a >= b && a >= c)
+    {
+        printf("%d", a);
+    }
+    else if (b >= a && b >= c)
+    {
+        printf("%d", b);
+    }
+    else if (c >= a && c >= b)
+    {
+        printf("%d", c);
     }
 
     return 0;
