29d761e2cc913169de0ad4e46575bce303587e6d
abdulhakim10 <s.abdulhakim10@gmail.com>
2025-05-01 14:33:15 +0800

    two problems solved loop (easy)

--- lesson_practice.c
+++ lesson_practice.c
@@ -1,11 +1,52 @@
-#include <stdio.h>
+// #include <stdio.h>
+// int main()
+// {
+//     int sum = 0;
+
+//     for(int i = 0; i <= 10; i++ ){
+//         sum = sum + i;
+//         printf("%d, %d\n", i, sum);
+//     }
+//     return  0;
+// }
+
+// post increment
+// #include<stdio.h>
+// int main()
+// {
+//     int x = 10;
+//     int y = x++;
+//     printf("%d %d", x, y);
+//     return 0;
+// }
+
+// pre increment
+// #include<stdio.h>
+// int main()
+// {
+//     int x = 10;
+//     int y = ++x;
+//     printf("%d %d", x, y);
+//     return 0;
+// }
+
+#include<stdio.h>
 int main()
 {
-    int sum = 0;
+    // int x = 10;
+    // int y = x++;
+    // int z = ++y;
+    // printf("%d", z++);
+    // printf("%d", z++);
 
-    for(int i = 0; i <= 10; i++ ){
-        sum = sum + i;
-        printf("%d, %d\n", i, sum);
-    }
-    return  0;
+    // int x = 10;
+    // int y = x--;
+    // int z = --y;
+    // printf("%d", z--);
+    // printf("%d", z--);
+
+    // int x = 10;
+    // ++x;
+    // printf("%d", ++x);
+    // return 0;
 }
